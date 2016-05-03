ce7eabee85755f67301060488f93a41e243103c0
camera360 <camera360@mazhongdeMacBook-Pro.local>
2016-05-03 16:08:56 +0800

    first commit

---
+++ TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by camera360 on 16/5/3.
+//  Copyright © 2016年 camera360. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestDemo
+//
+//  Created by camera360 on 16/5/3.
+//  Copyright © 2016年 camera360. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
